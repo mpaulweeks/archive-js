28c787b98d73ba5d3c6e11e91f7f8cedd688bbb2
mpaulweeks <mpaulweeks@gmail.com>
2021-12-28 10:18:20 -0500

    cleanup

--- src/youtube.ts
+++ src/youtube.ts
@@ -6,35 +6,8 @@ type YoutubeInfo = ytdl.videoInfo;
 
 export class YouTube {
   async getInfo(url: string): Promise<YoutubeInfo> {
-    // return new Promise((resolve, reject) => {
-    //   youtubedl.getInfo(url, options, function(err, info) {
-    //     if (err) throw err
-
-    //     console.log('id:', info.id)
-    //     console.log('title:', info.title)
-    //     console.log('url:', info.url)
-    //     console.log('thumbnail:', info.thumbnail)
-    //     console.log('description:', info.description)
-    //     console.log('filename:', info._filename)
-    //     console.log('format id:', info.format_id)
-    //   })
-    // })
     const info = await ytdl.getBasicInfo(url);
     return info;
-  }
-
-  async download(url: string): Promise<string> {
-    const info = await this.getInfo(url);
-    const filename = `${info.videoDetails.author.name} | ${info.videoDetails.title}.mp4`;
-    const readStream = ytdl(url);
-    const writeStream = fs.createWriteStream(filename);
-    const promise = new Promise<void>((resolve, reject) => {
-      writeStream.on('finish', resolve);
-      writeStream.on('error', reject);
-    });
-    readStream.pipe(writeStream);
-    await promise;
-    return filename;
   }
 
   async mux(url: string): Promise<string> {
